dfd25e635bf8e331177004afa57c1ae147bdd503
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2026-05-10 12:56:55 +0530

    Insert at beginning
    Also traversing nodes to print

--- linkedList.js
+++ linkedList.js
@@ -16,6 +16,17 @@ class LinkedList{
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  printList(){
+    let current = this.head;
+    let str = "";
+    while(current){
+      str = str + current.data + "-->";
+      current = current.next;
+    }
+
+    console.log(str + "null");
+  }
 }
 
 
@@ -25,5 +36,8 @@ class LinkedList{
 const list = new LinkedList();
 
 list.insertAtBeginning(18);
+list.insertAtBeginning(25);
 
-console.log(list);
+list.printList();
+
+// console.log(list);
